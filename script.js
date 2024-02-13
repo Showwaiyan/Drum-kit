b3d8cf87abcba431737c3d47eacb44af60ca5e54
Show Wai Yan <showwaiyan555@gmail.com>
2024-02-13 10:45:38 +0800

    Finished playSoundWithKey functionality

--- script.js
+++ script.js
@@ -8,3 +8,21 @@ function playSoundWithMouse(event) {
     if (event.target.className === "drum-set") new Audio(`sounds/${event.target.id}.wav`).play();
     else if (event.target.parentElement.className === "drum-set") new Audio(`sounds/${event.target.parentElement.id}.wav`).play();
 }
+
+
+document.addEventListener("keydown",playSoundWithKey);
+
+function playSoundWithKey(event){
+    const keyPair = {
+        A:"clap",
+        S:"hihat",
+        D:"kick",
+        F:"openhat",
+        G:"boom",
+        H:"ride",
+        J:"snare",
+        K:"tom",
+        L:"tink"
+    };
+    if(event.key.toUpperCase() in keyPair) new Audio(`sounds/${keyPair[event.key.toUpperCase()]}.wav`).play();
+}
