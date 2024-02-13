04a6ea94cba8d04e99daa83f883ce735bc745507
Show Wai Yan <showwaiyan555@gmail.com>
2024-02-13 10:33:26 +0800

    Change playSound function to playSoundWithMouse

--- script.js
+++ script.js
@@ -2,9 +2,9 @@
 
 const drumKit = document.querySelector(".drum-kit");
 
-drumKit.addEventListener("click",playSound)
+drumKit.addEventListener("click",playSoundWithMouse);
 
-function playSound(event) {
+function playSoundWithMouse(event) {
     if (event.target.className === "drum-set") new Audio(`sounds/${event.target.id}.wav`).play();
     else if (event.target.parentElement.className === "drum-set") new Audio(`sounds/${event.target.parentElement.id}.wav`).play();
 }
